85b4d68664047f3c7b05c1a58c400ccae769cb5f
Akshay Nisal <akshaynisal@outlook.com>
2024-01-02 00:12:15 +0530

    x509 demo

---
+++ x509Demo/src/main/java/com/akii/x509Demo/X509DemoApplication.java
@@ -0,0 +1,13 @@
+package com.akii.x509Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X509DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(X509DemoApplication.class, args);
+	}
+
+}
---
+++ x509Demo/src/test/java/com/akii/x509Demo/X509DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akii.x509Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class X509DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
